d896e546a1c06ec0f4e9186b71d9c5ad20c42a37
Proskurnikov <wapro@outlook.com>
2019-01-24 05:40:11 +0300

    Add first working version of BuitinObserver

---
+++ src/Observer/BuiltinObserver/WeatherStation/ObservableModel/Measurements.cs
@@ -0,0 +1,17 @@
+﻿namespace WeatherStation.ObservableModel
+{
+    struct Measurements
+    {
+        public float Temperature { get; private set; }
+        public float Humidity { get; private set; }
+        public float Pressure { get; private set; }
+
+        public Measurements(float temperature, float humidity, float pressure)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Pressure = pressure;
+        }
+
+    }
+}
---
+++ src/Observer/BuiltinObserver/WeatherStation/Observers/CurrentConditionObserver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using WeatherStation.ObservableModel;
+
+namespace WeatherStation.Observers
+{
+    class CurrentConditionObserver : IObserver<Measurements>
+    {
+        #region Variables
+
+        IDisposable unsubscriber;
+
+        #endregion Variables
+
+        #region Ctor
+
+        public CurrentConditionObserver(IObservable<Measurements> weatherData)
+        {
+            unsubscriber = weatherData?.Subscribe(this);
+        }
+
+        #endregion Ctor
+
+        #region IObserver
+
+        public void OnCompleted()
+        {
+            unsubscriber?.Dispose();
+        }
+
+        public void OnError(Exception error)
+        {
+            Console.WriteLine("Can't update weather");
+        }
+
+        public void OnNext(Measurements measurements)
+        {
+            Console.WriteLine($"Current: temperature {measurements.Temperature} ");
+        }
+
+        #endregion IObserver
+    }
+}
