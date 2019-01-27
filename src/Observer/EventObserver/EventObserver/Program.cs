6dd3b3e8167c6d34fc4b09a839163469c6125a0c
Proskurnikov <wapro@outlook.com>
2019-01-27 06:25:53 +0300

    Add observer using Event

---
+++ src/Observer/EventObserver/EventObserver/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EventObserver.WeatherData;
+using EventObserver.WeatherObservers;
+
+namespace EventObserver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            WeatherObservable weatherObservable = new WeatherObservable();
+            CurrentCondition currentCondition = new CurrentCondition(weatherObservable);
+
+            weatherObservable.GetMeasurements(new Measurements(10, 60, 760));
+            weatherObservable.GetMeasurements(null);
+
+            #if (!vscode) // Add this for run from VS in order to console window will keep open
+            Console.WriteLine("Press Enter for exit");
+            Console.ReadLine();
+            #endif
+        }
+    }
+}
